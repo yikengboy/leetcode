d279e4ce72231ed8d4e07700c2482627c1c61083
songke <songke@qunhemail.com>
2025-12-26 11:38:10 +0800

    20251226 移除链表元素

---
+++ src/main/java/com/yikengboy/Main.java
@@ -0,0 +1,13 @@
+package com.yikengboy;
+
+/**
+ * LeetCode 刷题项目主入口
+ *
+ * @author songke
+ * @date 2025/12/25
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/yikengboy/solutions/easy/RemoveLinkedTable.java
@@ -0,0 +1,48 @@
+package com.yikengboy.solutions.easy;
+
+/// [题目链接](https://leetcode.cn/problems/remove-linked-list-elements/description/)
+public class RemoveLinkedTable {
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode() {}
+     * ListNode(int val) { this.val = val; }
+     * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode header = new ListNode();
+        header.next = head;
+
+        ListNode cur = header;
+        while (cur.next != null) {
+            if (cur.next.val == val) {
+                cur.next = cur.next.next;
+            } else {
+                cur = cur.next;
+            }
+        }
+        return header.next;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
